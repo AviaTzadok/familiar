77e06faf704cbff3568dbd1ad9dfe10d602826b1
Yael Krig <yaelkrig@gmail.com>
2022-02-02 18:18:21 +0200

    starting level 1

--- src/components/levels/level1/Level1.js
+++ src/components/levels/level1/Level1.js
@@ -1,12 +1,41 @@
+import { useState } from 'react';
 import './Level1.css';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+import { Box } from '@mui/system';
+
 const Level1 = () => {
     const data = [{
-        name: "miri",
-        img: "./imagesEX/1.jpg"
+        name: "מירי",
+        img: 'https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_.jpg'
+    },
+    {
+        name: "מרים",
+        img: 'https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg'
     }]
-    return <div>
-        <img src={data[0].img} alt={data[0].name} />
-    </div>
+    const [index, setIndex] = useState(0)
+    // const time = setInterval(() => {
+    //     setIndex(index + 1)
+    // }, 7000)
+    console.log(index);
+    if (index === data.length - 1) {
+        // clearInterval(time)
+        console.log('clear');
+    }
+    return (
+        <div className='Level1'>
+            <div>
+                <img src={data[index].img} alt={data[index].name} className='image' />
+            </div>
+            <Box className="controlers">
+                <ArrowCircleLeftIcon
+                    onClick={() => { setIndex(index - 1) }} />
+                <span className='name'>{data[index].name}</span>
+                <ArrowCircleRightIcon
+                    onClick={() => { setIndex(index + 1) }} />
+            </Box>
+        </div>
+    )
 }
 
 export default Level1;
