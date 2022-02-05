4585dba7f25b7ae1e8ea29ff76a75a630c12a8de
Yael Krig <yaelkrig@gmail.com>
2022-02-05 20:43:09 +0200

    last week changes

--- src/components/levels/level1/Level1.js
+++ src/components/levels/level1/Level1.js
@@ -1,38 +1,63 @@
-import { useState } from 'react';
 import './Level1.css';
+import { useState } from 'react';
 import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
 import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+import CloseIcon from '@mui/icons-material/Close';
 import { Box } from '@mui/system';
 
 const Level1 = () => {
+    const [index, setIndex] = useState(0);
     const data = [{
         name: "מירי",
+        img: 'https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_.jpg'
+    },
+    {
+        name: "משה",
         img: 'https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_.jpg'
     },
     {
         name: "מרים",
         img: 'https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg'
     }]
-    const [index, setIndex] = useState(0)
-    // const time = setInterval(() => {
-    //     setIndex(index + 1)
-    // }, 7000)
-    console.log(index);
-    if (index === data.length - 1) {
-        // clearInterval(time)
-        console.log('clear');
+    const dataIndexes = data.length - 1;
+
+    const skip = () => {
+        console.log('skip', index);
+        if (index === dataIndexes) {
+            setIndex(0)
+        } else {
+            setIndex(index + 1)
+        }
     }
+    const previous = () => {
+        console.log('pre', index);
+        if (index === 0) {
+            console.log(data.length - 1);
+            setIndex(dataIndexes)
+        } else {
+            setIndex(index - 1)
+        }
+    }
+    const time = setInterval(skip, 7000)
+    const stop = () => {
+        clearInterval(time)
+    }
+    const timeStop = setTimeout(stop, dataIndexes * 7000)
+
+
     return (
         <div className='Level1'>
-            <div>
+            <div className='image_container'>
                 <img src={data[index].img} alt={data[index].name} className='image' />
             </div>
             <Box className="controlers">
                 <ArrowCircleLeftIcon
-                    onClick={() => { setIndex(index - 1) }} />
+                    onClick={() => { previous() }} />
                 <span className='name'>{data[index].name}</span>
                 <ArrowCircleRightIcon
-                    onClick={() => { setIndex(index + 1) }} />
+                    onClick={() => { skip() }} />
+                <CloseIcon
+                    onClick={() => { stop() }} />
             </Box>
         </div>
     )
